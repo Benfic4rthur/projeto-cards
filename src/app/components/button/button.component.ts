ec12ae58181bf41c431046476ccd34490aea4bce
benfic4rthur <arthur_benfica@hotmail.com>
2024-07-03 21:20:32 -0300

    mais um pouco de estudos.

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -6,9 +6,9 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
-  @Input({required: true, alias: 'buttonText'}) buttonText : string = '';
+  @Input({required: true, alias: 'buttonText'}) buttonText : string = ''; // recebe o texto do componente pai
   @Input({required: true, alias: 'buttonStyle'}) buttonStyle : 'white' | 'purple' = 'white';
-  @Output('buttonClickedEmitt') buttonClickedEmitt = new EventEmitter<void>();
+  @Output('buttonClickedEmitt') buttonClickedEmitt = new EventEmitter<void>(); // envia o evento para o componente pai
   onButtonClick(){
     this.buttonClickedEmitt.emit();
   }
