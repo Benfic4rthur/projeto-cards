8e1f3499d52b32bea51cd596a105482f06ced8c6
benfic4rthur <arthur_benfica@hotmail.com>
2024-07-04 18:22:17 -0300

    Avanços nos estudos em Angular.

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'] // Certifique-se de que o caminho está correto
+})
+
+export class CardComponent {
+  @Input({required: true, alias: 'cardText'}) cardText: string = ''; // recebe o texto do componente pai
+  @Input({required: true, alias: 'cardPrice'}) cardPrice: number = 0; // recebe o preço do componente pai
+  @Input({required: true, alias: 'cardStyle'}) cardStyle: 'orange' | 'green' = 'orange';
+}
